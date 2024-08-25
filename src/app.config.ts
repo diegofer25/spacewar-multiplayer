34c5537c3f4c759e3bc25c97948cc75980cad9e3
Diego Lamarão <diego.lamarao92@gmail.com>
2024-08-25 17:07:43 -0300

    remove client files from server

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,15 +1,11 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
-import express from "express";
 import basicAuth from "express-basic-auth";
 
 /**
  * Import your Room files
  */
 import { Game } from "./rooms/Game";
-import path from "path";
-
-const distPath = path.join(__dirname, "./../client/dist");
 
 export default config({
 
@@ -21,8 +17,6 @@ export default config({
     },
 
     initializeExpress: (app) => {
-        // add static files
-        app.use("/", express.static(distPath));
 
         const basicAuthMiddleware = basicAuth({
             // list of users and passwords
@@ -34,11 +28,6 @@ export default config({
             challenge: true
         });
         app.use("/colyseus", basicAuthMiddleware, monitor());
-
-        // redirect every other route to index.html
-        app.get("*", (req, res) => {
-            res.sendFile(path.resolve(distPath, "index.html"));
-        });
     },
 
 
