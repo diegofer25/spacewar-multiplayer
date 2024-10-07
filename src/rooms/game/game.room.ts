e0e8167fb0a61d139b75e6e07e3507a63c27179c
Diego Lamarão <diego.lamarao92@gmail.com>
2024-10-07 15:20:14 -0300

    fix server intersecting logic

--- src/rooms/game/game.room.ts
+++ src/rooms/game/game.room.ts
@@ -325,10 +325,8 @@ export class Game extends Room<GameState> {
 
     private isIntersecting(rect1: Rectangle, rect2: Rectangle) {
         return (
-            rect1.x < rect2.x + rect2.width &&
-            rect1.x + rect1.width > rect2.x &&
-            rect1.y < rect2.y + rect2.height &&
-            rect1.y + rect1.height > rect2.y
+            Math.abs(rect1.x - rect2.x) * 2 < rect1.width + rect2.width &&
+            Math.abs(rect1.y - rect2.y) * 2 < rect1.height + rect2.height
         );
     }
 }
