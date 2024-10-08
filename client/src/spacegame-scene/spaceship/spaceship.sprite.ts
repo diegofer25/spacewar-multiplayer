222ae6410c8d21a620974affe6e83d5c915e1fd1
Diego Lamarão <diego.lamarao92@gmail.com>
2024-10-08 11:21:45 -0300

    roll back position update change

--- client/src/spacegame-scene/spaceship/spaceship.sprite.ts
+++ client/src/spacegame-scene/spaceship/spaceship.sprite.ts
@@ -191,13 +191,11 @@ export class SpaceshipSprite extends Phaser.Physics.Arcade.Sprite implements Gam
                 this.removePowerUpEffect();
             }
         }
-        if (GameRoom.latency > 128) {
-            if (Math.abs(this.x - spaceship.x) > 50 || Math.abs(this.y - spaceship.y) > 50) {
-                this.setPosition(spaceship.x, spaceship.y);
-            }
-            if (Math.abs(this.rotation - spaceship.rotation) > 0.2) {
-                this.setRotation(spaceship.rotation);
-            }
+        if (Math.abs(this.x - spaceship.x) > 50 || Math.abs(this.y - spaceship.y) > 50) {
+            this.setPosition(spaceship.x, spaceship.y);
+        }
+        if (Math.abs(this.rotation - spaceship.rotation) > 0.1) {
+            this.setRotation(spaceship.rotation);
         }
     }
 
