97e5c6ffe8fe17ea7999a2d651b972d42bfb0f0f
Diego Lamarão <diego.lamarao92@gmail.com>
2024-10-12 11:13:25 -0300

    fix viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import svgLoader from 'vite-svg-loader';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), svgLoader()],
     resolve: {
         alias: {
-            client: path.resolve(__dirname, './src'),
+            client: path.resolve(__dirname, 'src'),
             server: path.resolve(__dirname, '../src'),
             'shared-configs': path.resolve(__dirname, '../shared-configs.json'),
         },
