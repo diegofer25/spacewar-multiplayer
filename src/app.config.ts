cb4950d9490380b50b76d08c4822ef87b3fb04b4
Diego Lamarão <diego.lamarao92@gmail.com>
2024-10-14 09:35:25 -0300

    add monitor configs

--- src/app.config.ts
+++ src/app.config.ts
@@ -25,13 +25,13 @@ export default config({
         const basicAuthMiddleware = basicAuth({
             // list of users and passwords
             users: {
-                admin: 'admin',
+                [process.env.MONITOR_USER]: process.env.MONITOR_PASSWORD,
             },
             // sends WWW-Authenticate header, which will prompt the user to fill
             // credentials in
             challenge: true,
         });
-        app.use('/colyseus', basicAuthMiddleware, monitor());
+        app.use('/monitor', basicAuthMiddleware, monitor());
     },
 
     beforeListen: () => {
